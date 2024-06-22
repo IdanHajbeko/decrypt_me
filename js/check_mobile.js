d00416be3d0e566f1cd7fa80fb3cf890d63be8f9
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-06-22 04:16:33 +0300

    Delete js directory

--- js/check_mobile.js
+++
@@ -1,9 +0,0 @@
-function isMobileDevice() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    return /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(userAgent) && !/ipad/i.test(userAgent);
-}
-
-if (isMobileDevice()) {
-    window.location.href = "please_dont_use_mobile/index.html";
-}
