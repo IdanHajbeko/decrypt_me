2e5ace6b402b3afc73958d3719aace705da92468
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-06-22 04:14:02 +0300

    Add files via upload

---
+++ please_dont_use_mobile/check_mobile.js
@@ -0,0 +1,9 @@
+function isMobileDevice() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    return /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(userAgent) && !/ipad/i.test(userAgent);
+}
+
+if (isMobileDevice()) {
+    window.location.href = "index.html";
+}
