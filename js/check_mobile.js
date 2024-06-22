e0882b303e18afc0a5e993900b1c32b1e5483986
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2024-06-22 04:01:03 +0300

    Update check_mobile.js

--- js/check_mobile.js
+++ js/check_mobile.js
@@ -1,7 +1,6 @@
 function isMobileDevice() {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    // Check for common mobile user agent strings excluding iPad
     return /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(userAgent) && !/ipad/i.test(userAgent);
 }
 
